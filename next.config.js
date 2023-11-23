7cc483ca463b66e5b958c02b028c6c57843f4d0b
Saeed-Khazaei <saeedkhazaei94@google.com>
2023-11-23 21:16:33 +0330

    feat(index): implement user component in index page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+const { hostname } = require("os");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+        pathname: '**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
